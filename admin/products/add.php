15f0bbfed7968d9df3338f18ffc5d9fb295cf03e
ldtrong <trondle258@gmail.com>
2021-01-24 19:49:24 +0700

    done.

--- admin/products/add.php
+++ admin/products/add.php
@@ -87,7 +87,7 @@
 
                       <div class="col-md-3">
                          <input class="form-control input-sm" id="PROPRICE"  step="any" name="PROPRICE" placeholder=
-                            "&#8369 Price " type="number" value="">
+                            "$ Price " type="number" value="">
                       </div>
                     </div>
                   </div>
--- include/config.php
+++ include/config.php
@@ -1,16 +1,16 @@
 <?php
 
 //Development connection
-// defined('server') ? null : define("server", "localhost");
-// defined('user') ? null : define ("user", "root") ;
-// defined('pass') ? null : define("pass","");
-// defined('database_name') ? null : define("database_name", "db_ecommerce") ;
+defined('server') ? null : define("server", "localhost");
+defined('user') ? null : define ("user", "root") ;
+defined('pass') ? null : define("pass","");
+defined('database_name') ? null : define("database_name", "db_ecommerce") ;
 
 //Remote database connection
-defined('server') ? null : define("server", "remotemysql.com");
-defined('user') ? null : define ("user", "Geuku7eZP5") ;
-defined('pass') ? null : define("pass","LtA2FwwuL5");
-defined('database_name') ? null : define("database_name", "Geuku7eZP5") ;
+// defined('server') ? null : define("server", "remotemysql.com");
+// defined('user') ? null : define ("user", "Geuku7eZP5") ;
+// defined('pass') ? null : define("pass","LtA2FwwuL5");
+// defined('database_name') ? null : define("database_name", "Geuku7eZP5") ;
 
 $this_file = str_replace('\\', '/', __File__) ;
 $doc_root = $_SERVER['DOCUMENT_ROOT'];
